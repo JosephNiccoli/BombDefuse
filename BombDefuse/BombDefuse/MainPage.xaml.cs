f5a1b4c7fa91a5ada9cda7ce8be5a68caa278585
joe niccoli <niccoli.joseph@gmail.com>
2019-03-07 12:49:30 -0800

    final commit

--- BombDefuse/BombDefuse/MainPage.xaml.cs
+++ BombDefuse/BombDefuse/MainPage.xaml.cs
@@ -9,9 +9,34 @@ namespace BombDefuse
 {
 	public partial class MainPage : ContentPage
 	{
+
+		static string bomb = new Random().Next(1, 4).ToString();
+		static int score = 0;
+
 		public MainPage()
 		{
 			InitializeComponent();
+		}
+		// when the user clicks the button // buttonClicked is a method more specifically an event method
+		async void ButtonClicked(object sender, EventArgs e) // event handler object sender contains the refrence to the button that was clicked and eventArgs contains the data asociated with the object
+		{
+			Button button = sender as Button; // cast as a button type
+
+			// Game Over
+
+			if (button.Text == bomb)
+			{
+				await DisplayAlert("Bomb Exploded", "GAME OVER!!!!!", "RETRY");
+				bomb = new Random().Next(1, 4).ToString();
+				score = 0;
+			}
+			else
+			{
+				score += 1;
+				await DisplayAlert("Defused Bomb!!!", "Scores: " + score, "Continue");
+				bomb = new Random().Next(1, 4).ToString();
+
+			}
 		}
 	}
 }
